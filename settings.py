862c50ad36b35e0896b0ab9282e70c7795800b1e
AlinaGay <gayfullina.a@gmail.com>
2025-08-25 22:20:29 +0200

    Add migrations

--- settings.py
+++ settings.py
@@ -4,6 +4,4 @@ import os
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
-
-
-config = Config()
+    DISK_TOKEN = os.getenv("DISK_TOKEN")
