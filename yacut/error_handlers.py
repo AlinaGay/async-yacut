db68f08e9b18c54fc97e9d84a4698beaa18da61b
AlinaGay <gayfullina.a@gmail.com>
2025-08-28 11:50:17 +0200

    Add error templates

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -26,7 +26,7 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 
-@app.errorhandler(500)
-def internal_error(error):
+@app.errorhandler(400)
+def bad_request(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('400.html'), 400
